4d6a1af924b5ded39923e161789ef6c9fbe496a7
Matthieu Boyer <mb692906@gmail.com>
2020-01-24 22:15:32 +0100

    Copypasta for new items
    All wood-derivated items + iron tools with a brand new un-bugged syntax ! yay

--- src/main/java/io/github/vampirestudios/molecularcraft/enums/ItemMolecules.java
+++ src/main/java/io/github/vampirestudios/molecularcraft/enums/ItemMolecules.java
@@ -86,126 +86,313 @@ public class ItemMolecules {
                 celluloseMolecule.setAmount(new MoleculeAmount(4, BLOCK).getAmount()));
 
         new ItemMolecules("minecraft:acacia_planks",
-                new MoleculeStack(new MoleculeAmount(9, INGOT), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(1, BLOCK).getAmount()));
 
         new ItemMolecules("minecraft:acacia_button",
-                new MoleculeStack(new MoleculeAmount(2, NUGGET), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(4, NUGGET).getAmount()));
 
         new ItemMolecules("minecraft:acacia_door",
-                new MoleculeStack(new MoleculeAmount(6, INGOT), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(6, INGOT).getAmount()));
 
         new ItemMolecules("minecraft:acacia_slab",
-                new MoleculeStack(new MoleculeAmount(2, INGOT), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(2, INGOT).getAmount()));
 
         new ItemMolecules("minecraft:acacia_stairs",
-                new MoleculeStack(new MoleculeAmount(6, INGOT), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(6, INGOT).getAmount()));
 
         new ItemMolecules("minecraft:acacia_pressure_plate",
-                new MoleculeStack(new MoleculeAmount(2, INGOT), celluloseMolecule));
-
-        new ItemMolecules("minecraft:acacia_button",
-                new MoleculeStack(new MoleculeAmount(4, NUGGET), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(2, INGOT).getAmount()));
 
         new ItemMolecules("minecraft:acacia_boat",
-                new MoleculeStack(new MoleculeAmount(5, INGOT), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(5, INGOT).getAmount()));
 
         new ItemMolecules("minecraft:acacia_fence",
-                new MoleculeStack(new MoleculeAmount(64, NUGGET), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(9, INGOT).getAmount()));
 
         new ItemMolecules("minecraft:acacia_fence_gate",
-                new MoleculeStack(new MoleculeAmount(64, NUGGET), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(8, INGOT).getAmount()));
 
         new ItemMolecules("minecraft:acacia_leaves",
-                new MoleculeStack(new MoleculeAmount(27, INGOT), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(12, NUGGET).getAmount()));
 
         new ItemMolecules("minecraft:acacia_sapling",
-                new MoleculeStack(new MoleculeAmount(6, INGOT), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(4, INGOT).getAmount()));
 
         new ItemMolecules("minecraft:acacia_wood",
-                new MoleculeStack(new MoleculeAmount(82, NUGGET), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(5, BLOCK).getAmount()));
 
         new ItemMolecules("minecraft:oak_log",
-                new MoleculeStack(new MoleculeAmount(4, BLOCK), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(4, BLOCK).getAmount()));
 
         new ItemMolecules("minecraft:oak_planks",
-                new MoleculeStack(new MoleculeAmount(9, INGOT), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(1, BLOCK).getAmount()));
 
         new ItemMolecules("minecraft:oak_button",
-                new MoleculeStack(new MoleculeAmount(2, NUGGET), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(4, NUGGET).getAmount()));
 
         new ItemMolecules("minecraft:oak_door",
-                new MoleculeStack(new MoleculeAmount(6, INGOT), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(6, INGOT).getAmount()));
 
         new ItemMolecules("minecraft:oak_slab",
-                new MoleculeStack(new MoleculeAmount(2, INGOT), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(2, INGOT).getAmount()));
 
         new ItemMolecules("minecraft:oak_stairs",
-                new MoleculeStack(new MoleculeAmount(6, INGOT), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(6, INGOT).getAmount()));
 
         new ItemMolecules("minecraft:oak_pressure_plate",
-                new MoleculeStack(new MoleculeAmount(2, INGOT), celluloseMolecule));
-
-        new ItemMolecules("minecraft:oak_button",
-                new MoleculeStack(new MoleculeAmount(4, NUGGET), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(2, INGOT).getAmount()));
 
         new ItemMolecules("minecraft:oak_boat",
-                new MoleculeStack(new MoleculeAmount(5, INGOT), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(5, INGOT).getAmount()));
 
         new ItemMolecules("minecraft:oak_fence",
-                new MoleculeStack(new MoleculeAmount(64, NUGGET), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(9, INGOT).getAmount()));
 
         new ItemMolecules("minecraft:oak_fence_gate",
-                new MoleculeStack(new MoleculeAmount(64, NUGGET), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(8, INGOT).getAmount()));
 
         new ItemMolecules("minecraft:oak_leaves",
-                new MoleculeStack(new MoleculeAmount(27, INGOT), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(12, NUGGET).getAmount()));
 
         new ItemMolecules("minecraft:oak_sapling",
-                new MoleculeStack(new MoleculeAmount(6, INGOT), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(4, INGOT).getAmount()));
 
         new ItemMolecules("minecraft:oak_wood",
-                new MoleculeStack(new MoleculeAmount(82, NUGGET), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(5, BLOCK).getAmount()));
+
+        new ItemMolecules("minecraft:dark_oak_log",
+                celluloseMolecule.setAmount(new MoleculeAmount(4, BLOCK).getAmount()));
+
+        new ItemMolecules("minecraft:dark_oak_planks",
+                celluloseMolecule.setAmount(new MoleculeAmount(1, BLOCK).getAmount()));
+
+        new ItemMolecules("minecraft:dark_oak_button",
+                celluloseMolecule.setAmount(new MoleculeAmount(4, NUGGET).getAmount()));
+
+        new ItemMolecules("minecraft:dark_oak_door",
+                celluloseMolecule.setAmount(new MoleculeAmount(6, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:dark_oak_slab",
+                celluloseMolecule.setAmount(new MoleculeAmount(2, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:dark_oak_stairs",
+                celluloseMolecule.setAmount(new MoleculeAmount(6, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:dark_oak_pressure_plate",
+                celluloseMolecule.setAmount(new MoleculeAmount(2, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:dark_oak_boat",
+                celluloseMolecule.setAmount(new MoleculeAmount(5, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:dark_oak_fence",
+                celluloseMolecule.setAmount(new MoleculeAmount(9, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:dark_oak_fence_gate",
+                celluloseMolecule.setAmount(new MoleculeAmount(8, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:dark_oak_leaves",
+                celluloseMolecule.setAmount(new MoleculeAmount(12, NUGGET).getAmount()));
+
+        new ItemMolecules("minecraft:dark_oak_sapling",
+                celluloseMolecule.setAmount(new MoleculeAmount(4, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:dark_oak_wood",
+                celluloseMolecule.setAmount(new MoleculeAmount(5, BLOCK).getAmount()));
 
         new ItemMolecules("minecraft:birch_log",
-                new MoleculeStack(new MoleculeAmount(4, BLOCK), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(4, BLOCK).getAmount()));
 
         new ItemMolecules("minecraft:birch_planks",
-                new MoleculeStack(new MoleculeAmount(9, INGOT), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(1, BLOCK).getAmount()));
 
         new ItemMolecules("minecraft:birch_button",
-                new MoleculeStack(new MoleculeAmount(2, NUGGET), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(4, NUGGET).getAmount()));
 
         new ItemMolecules("minecraft:birch_door",
-                new MoleculeStack(new MoleculeAmount(6, INGOT), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(6, INGOT).getAmount()));
 
         new ItemMolecules("minecraft:birch_slab",
-                new MoleculeStack(new MoleculeAmount(2, INGOT), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(2, INGOT).getAmount()));
 
         new ItemMolecules("minecraft:birch_stairs",
-                new MoleculeStack(new MoleculeAmount(6, INGOT), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(6, INGOT).getAmount()));
 
         new ItemMolecules("minecraft:birch_pressure_plate",
-                new MoleculeStack(new MoleculeAmount(2, INGOT), celluloseMolecule));
-
-        new ItemMolecules("minecraft:birch_button",
-                new MoleculeStack(new MoleculeAmount(4, NUGGET), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(2, INGOT).getAmount()));
 
         new ItemMolecules("minecraft:birch_boat",
-                new MoleculeStack(new MoleculeAmount(5, INGOT), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(5, INGOT).getAmount()));
 
         new ItemMolecules("minecraft:birch_fence",
-                new MoleculeStack(new MoleculeAmount(64, NUGGET), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(9, INGOT).getAmount()));
 
         new ItemMolecules("minecraft:birch_fence_gate",
-                new MoleculeStack(new MoleculeAmount(64, NUGGET), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(8, INGOT).getAmount()));
 
         new ItemMolecules("minecraft:birch_leaves",
-                new MoleculeStack(new MoleculeAmount(27, INGOT), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(12, NUGGET).getAmount()));
 
         new ItemMolecules("minecraft:birch_sapling",
-                new MoleculeStack(new MoleculeAmount(6, INGOT), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(4, INGOT).getAmount()));
 
         new ItemMolecules("minecraft:birch_wood",
-                new MoleculeStack(new MoleculeAmount(82, NUGGET), celluloseMolecule));
+                celluloseMolecule.setAmount(new MoleculeAmount(5, BLOCK).getAmount()));
+
+        new ItemMolecules("minecraft:jungle_log",
+                celluloseMolecule.setAmount(new MoleculeAmount(4, BLOCK).getAmount()));
+
+        new ItemMolecules("minecraft:jungle_planks",
+                celluloseMolecule.setAmount(new MoleculeAmount(1, BLOCK).getAmount()));
+
+        new ItemMolecules("minecraft:jungle_button",
+                celluloseMolecule.setAmount(new MoleculeAmount(4, NUGGET).getAmount()));
+
+        new ItemMolecules("minecraft:jungle_door",
+                celluloseMolecule.setAmount(new MoleculeAmount(6, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:jungle_slab",
+                celluloseMolecule.setAmount(new MoleculeAmount(2, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:jungle_stairs",
+                celluloseMolecule.setAmount(new MoleculeAmount(6, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:jungle_pressure_plate",
+                celluloseMolecule.setAmount(new MoleculeAmount(2, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:jungle_boat",
+                celluloseMolecule.setAmount(new MoleculeAmount(5, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:jungle_fence",
+                celluloseMolecule.setAmount(new MoleculeAmount(9, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:jungle_fence_gate",
+                celluloseMolecule.setAmount(new MoleculeAmount(8, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:jungle_leaves",
+                celluloseMolecule.setAmount(new MoleculeAmount(12, NUGGET).getAmount()));
+
+        new ItemMolecules("minecraft:jungle_sapling",
+                celluloseMolecule.setAmount(new MoleculeAmount(4, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:jungle_wood",
+                celluloseMolecule.setAmount(new MoleculeAmount(5, BLOCK).getAmount()));
+
+        new ItemMolecules("minecraft:spruce_log",
+                celluloseMolecule.setAmount(new MoleculeAmount(4, BLOCK).getAmount()));
+
+        new ItemMolecules("minecraft:spruce_planks",
+                celluloseMolecule.setAmount(new MoleculeAmount(1, BLOCK).getAmount()));
+
+        new ItemMolecules("minecraft:spruce_button",
+                celluloseMolecule.setAmount(new MoleculeAmount(4, NUGGET).getAmount()));
+
+        new ItemMolecules("minecraft:spruce_door",
+                celluloseMolecule.setAmount(new MoleculeAmount(6, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:spruce_slab",
+                celluloseMolecule.setAmount(new MoleculeAmount(2, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:spruce_stairs",
+                celluloseMolecule.setAmount(new MoleculeAmount(6, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:spruce_pressure_plate",
+                celluloseMolecule.setAmount(new MoleculeAmount(2, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:spruce_boat",
+                celluloseMolecule.setAmount(new MoleculeAmount(5, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:spruce_fence",
+                celluloseMolecule.setAmount(new MoleculeAmount(9, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:spruce_fence_gate",
+                celluloseMolecule.setAmount(new MoleculeAmount(8, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:spruce_leaves",
+                celluloseMolecule.setAmount(new MoleculeAmount(12, NUGGET).getAmount()));
+
+        new ItemMolecules("minecraft:spruce_sapling",
+                celluloseMolecule.setAmount(new MoleculeAmount(4, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:spruce_wood",
+                celluloseMolecule.setAmount(new MoleculeAmount(5, BLOCK).getAmount()));
+
+        new ItemMolecules("minecraft:stripped_acacia_log",
+                celluloseMolecule.setAmount(new MoleculeAmount(3, BLOCK).getAmount()));
+
+        new ItemMolecules("minecraft:stripped_oak_log",
+                celluloseMolecule.setAmount(new MoleculeAmount(3, BLOCK).getAmount()));
+
+        new ItemMolecules("minecraft:stripped_dark_oak_log",
+                celluloseMolecule.setAmount(new MoleculeAmount(3, BLOCK).getAmount()));
+
+        new ItemMolecules("minecraft:stripped_jungle_log",
+                celluloseMolecule.setAmount(new MoleculeAmount(3, BLOCK).getAmount()));
+
+        new ItemMolecules("minecraft:stripped_spruce_log",
+                celluloseMolecule.setAmount(new MoleculeAmount(3, BLOCK).getAmount()));
+
+        new ItemMolecules("minecraft:stripped_birch_log",
+                celluloseMolecule.setAmount(new MoleculeAmount(3, BLOCK).getAmount()));
+
+        new ItemMolecules("minecraft:stripped_acacia_wood",
+                celluloseMolecule.setAmount(new MoleculeAmount(4, BLOCK).getAmount()));
+
+        new ItemMolecules("minecraft:stripped_acacia_wood",
+                celluloseMolecule.setAmount(new MoleculeAmount(4, BLOCK).getAmount()));
+
+        new ItemMolecules("minecraft:stripped_oak_wood",
+                celluloseMolecule.setAmount(new MoleculeAmount(4, BLOCK).getAmount()));
+
+        new ItemMolecules("minecraft:stripped_dark_oak_wood",
+                celluloseMolecule.setAmount(new MoleculeAmount(4, BLOCK).getAmount()));
+
+        new ItemMolecules("minecraft:stripped_jungle_wood",
+                celluloseMolecule.setAmount(new MoleculeAmount(4, BLOCK).getAmount()));
+
+        new ItemMolecules("minecraft:stripped_spruce_wood",
+                celluloseMolecule.setAmount(new MoleculeAmount(4, BLOCK).getAmount()));
+
+        new ItemMolecules("minecraft:stripped_birch_wood",
+                celluloseMolecule.setAmount(new MoleculeAmount(4, BLOCK).getAmount()));
+
+        new ItemMolecules("minecraft:stick",
+                celluloseMolecule.setAmount(new MoleculeAmount(2, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:wooden_axe",
+                celluloseMolecule.setAmount(new MoleculeAmount(7, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:wooden_hoe",
+                celluloseMolecule.setAmount(new MoleculeAmount(6, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:wooden_pickaxe",
+                celluloseMolecule.setAmount(new MoleculeAmount(7, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:wooden_shovel",
+                celluloseMolecule.setAmount(new MoleculeAmount(5, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:wooden_sword",
+                celluloseMolecule.setAmount(new MoleculeAmount(4, INGOT).getAmount()));
+
+        new ItemMolecules("minecraft:iron_axe",
+                celluloseMolecule.setAmount(new MoleculeAmount(4, INGOT).getAmount()),
+                new MoleculeStack(new MoleculeAmount(3,INGOT),ironMolecule));
+
+        new ItemMolecules("minecraft:iron_pickaxe",
+                celluloseMolecule.setAmount(new MoleculeAmount(4, INGOT).getAmount()),
+                new MoleculeStack(new MoleculeAmount(3,INGOT),ironMolecule));
+
+        new ItemMolecules("minecraft:iron_hoe",
+                celluloseMolecule.setAmount(new MoleculeAmount(4, INGOT).getAmount()),
+                new MoleculeStack(new MoleculeAmount(2,INGOT),ironMolecule));
+
+        new ItemMolecules("minecraft:iron_shovel",
+                celluloseMolecule.setAmount(new MoleculeAmount(4, INGOT).getAmount()),
+                new MoleculeStack(new MoleculeAmount(2,INGOT),ironMolecule));
+
+        new ItemMolecules("minecraft:iron_sword",
+                celluloseMolecule.setAmount(new MoleculeAmount(2, INGOT).getAmount()),
+                new MoleculeStack(new MoleculeAmount(2,INGOT),ironMolecule));
+
+
     }
 }
