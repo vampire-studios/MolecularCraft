e4d597177055da1719145811d3e6c33bb71f7d57
Matthieu Boyer <mb692906@gmail.com>
2020-01-24 20:57:05 +0100

    Adding all wood items molecules
    oh boy there's a lot
    and it's bugged for now
    oskour

--- src/main/java/io/github/vampirestudios/molecularcraft/enums/ItemMolecules.java
+++ src/main/java/io/github/vampirestudios/molecularcraft/enums/ItemMolecules.java
@@ -3,6 +3,7 @@ package io.github.vampirestudios.molecularcraft.enums;
 import io.github.vampirestudios.molecularcraft.molecules.Molecule;
 import io.github.vampirestudios.molecularcraft.molecules.MoleculeStack;
 import io.github.vampirestudios.molecularcraft.enums.MoleculesAmountHelper.MoleculeAmount;
+import net.minecraft.item.Item;
 
 import java.util.*;
 
@@ -81,5 +82,88 @@ public class ItemMolecules {
         new ItemMolecules("minecraft:iron_trapdoor",
                 new MoleculeStack(new MoleculeAmount(4, INGOT), ironMolecule));
 
+        new ItemMolecules("minecraft:acacia_log",
+                new MoleculeStack(new MoleculeAmount(4, BLOCK), celluloseMolecule));
+
+        new ItemMolecules("minecraft:acacia_planks",
+                new MoleculeStack(new MoleculeAmount(9, INGOT), celluloseMolecule));
+
+        new ItemMolecules("minecraft:acacia_button",
+                new MoleculeStack(new MoleculeAmount(2, NUGGET), celluloseMolecule));
+
+        new ItemMolecules("minecraft:acacia_door",
+                new MoleculeStack(new MoleculeAmount(6, INGOT), celluloseMolecule));
+
+        new ItemMolecules("minecraft:acacia_slab",
+                new MoleculeStack(new MoleculeAmount(2, INGOT), celluloseMolecule));
+
+        new ItemMolecules("minecraft:acacia_stairs",
+                new MoleculeStack(new MoleculeAmount(6, INGOT), celluloseMolecule));
+
+        new ItemMolecules("minecraft:acacia_pressure_plate",
+                new MoleculeStack(new MoleculeAmount(2, INGOT), celluloseMolecule));
+
+        new ItemMolecules("minecraft:acacia_button",
+                new MoleculeStack(new MoleculeAmount(4, NUGGET), celluloseMolecule));
+
+        new ItemMolecules("minecraft:acacia_boat",
+                new MoleculeStack(new MoleculeAmount(5, INGOT), celluloseMolecule));
+
+        new ItemMolecules("minecraft:acacia_fence",
+                new MoleculeStack(new MoleculeAmount(64, NUGGET), celluloseMolecule));
+
+        new ItemMolecules("minecraft:acacia_fence_gate",
+                new MoleculeStack(new MoleculeAmount(64, NUGGET), celluloseMolecule));
+
+        new ItemMolecules("minecraft:acacia_leaves",
+                new MoleculeStack(new MoleculeAmount(27, INGOT), celluloseMolecule));
+
+        new ItemMolecules("minecraft:acacia_sapling",
+                new MoleculeStack(new MoleculeAmount(6, INGOT), celluloseMolecule));
+
+        new ItemMolecules("minecraft:acacia_wood",
+                new MoleculeStack(new MoleculeAmount(82, NUGGET), celluloseMolecule));
+
+        new ItemMolecules("minecraft:oak_log",
+                new MoleculeStack(new MoleculeAmount(4, BLOCK), celluloseMolecule));
+
+        new ItemMolecules("minecraft:oak_planks",
+                new MoleculeStack(new MoleculeAmount(9, INGOT), celluloseMolecule));
+
+        new ItemMolecules("minecraft:oak_button",
+                new MoleculeStack(new MoleculeAmount(2, NUGGET), celluloseMolecule));
+
+        new ItemMolecules("minecraft:oak_door",
+                new MoleculeStack(new MoleculeAmount(6, INGOT), celluloseMolecule));
+
+        new ItemMolecules("minecraft:oak_slab",
+                new MoleculeStack(new MoleculeAmount(2, INGOT), celluloseMolecule));
+
+        new ItemMolecules("minecraft:oak_stairs",
+                new MoleculeStack(new MoleculeAmount(6, INGOT), celluloseMolecule));
+
+        new ItemMolecules("minecraft:oak_pressure_plate",
+                new MoleculeStack(new MoleculeAmount(2, INGOT), celluloseMolecule));
+
+        new ItemMolecules("minecraft:oak_button",
+                new MoleculeStack(new MoleculeAmount(4, NUGGET), celluloseMolecule));
+
+        new ItemMolecules("minecraft:oak_boat",
+                new MoleculeStack(new MoleculeAmount(5, INGOT), celluloseMolecule));
+
+        new ItemMolecules("minecraft:oak_fence",
+                new MoleculeStack(new MoleculeAmount(64, NUGGET), celluloseMolecule));
+
+        new ItemMolecules("minecraft:oak_fence_gate",
+                new MoleculeStack(new MoleculeAmount(64, NUGGET), celluloseMolecule));
+
+        new ItemMolecules("minecraft:oak_leaves",
+                new MoleculeStack(new MoleculeAmount(27, INGOT), celluloseMolecule));
+
+        new ItemMolecules("minecraft:oak_sapling",
+                new MoleculeStack(new MoleculeAmount(6, INGOT), celluloseMolecule));
+
+        new ItemMolecules("minecraft:oak_wood",
+                new MoleculeStack(new MoleculeAmount(82, NUGGET), celluloseMolecule));
     }
 }
