adbfdc91296324db6e535853de90f40a3615c22a
Cat Core <arthur.bambou@gmail.com>
2021-01-13 20:53:22 +0100

    add another needed molecule

--- src/main/java/io/github/vampirestudios/molecularcraft/config/MoleculeDataConfig.java
+++ src/main/java/io/github/vampirestudios/molecularcraft/config/MoleculeDataConfig.java
@@ -129,6 +129,7 @@ public class MoleculeDataConfig extends DataConfig {
         list.add(new MoleculeStack(new Molecule(CARBON, 28), new Molecule(HYDROGEN, 48), new Molecule(NITROGEN, 2), new Molecule(OXYGEN, 32), new Molecule(SULFUR, 4)));
         list.add(new MoleculeStack(new Molecule(POTASSIUM), new Molecule(NITROGEN), new Molecule(OXYGEN, 3)));
         list.add(new MoleculeStack(new Molecule(BERYLLIUM, 3), new Molecule(ALUMINIUM, 2), new Molecule(SILICON, 16), new Molecule(OXYGEN, 18)));
+        list.add(new MoleculeStack(new Molecule(CARBON, 42), new Molecule(HYDROGEN, 92), new Molecule(OXYGEN, 2)));
 
         return list;
     }
